94c6af002fb2eb088088f1860d91431a27e40be2
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-03-27 15:51:39 +0200

    Use port specified in environment

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
-const port = 3005;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
